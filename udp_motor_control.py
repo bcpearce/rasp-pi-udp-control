a968a26c1a27cb0ad922a97703e4081a435b4f64
Benjamin Pearce <bpearce9@gatech.edu>
2016-05-04 18:25:25 -0400

    test pwm code

--- udp_motor_control.py
+++ udp_motor_control.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python
+import RPi.GPIO as GPIO
+
+if __name__ == "__main__":
+
+    PIN_DICT = {'ENA':21, 'ENB':16, 
+                'IN1':13, 'IN2':20, 
+                'IN3':26, 'IN4':19}
+
+    GPIO.setmode(GPIO.BCM)
+
+    # all pins are output
+    for val in PIN_DICT.values():
+        GPIO.setup(val, GPIO.OUT)
+
+    # run at 50Hz
+    servo1 = GPIO.PWM(PIN_DICT['ENA'], 50)
+    servo2 = GPIO.PWM(PIN_DICT['ENB'], 50)
+
+    servo1.start(50.0)
+    servo2.start(50.0)
