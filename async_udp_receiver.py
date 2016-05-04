3c3cd420bebb877ce7223e735dc209ef1b1cf21e
Benjamin Pearce <bpearce9@gatech.edu>
2016-05-04 19:41:37 -0400

    added debugging

--- async_udp_receiver.py
+++ async_udp_receiver.py
@@ -2,15 +2,17 @@ import asyncore, socket, sys
 
 class AsyncUdpReceiver(asyncore.dispatcher):
 
-    def __init__(self, host, port, rservo, lservo):
+    def __init__(self, host, port, rservo=None, lservo=None):
         asyncore.dispatcher.__init__(self)
         # open as udp socket
         self.create_socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.bind((host, port))
-        self.rservo = rservo
-        self.lservo = lservo
-        self.rservo.start(0)
-        self.lservo.start(0)
+        if rservo:
+            self.rservo = rservo
+            self.rservo.start(0)
+        if lservo:
+            self.lservo = lservo 
+            self.lservo.start(0)
 
     def handle_connect(self):
         pass
@@ -25,13 +27,16 @@ class AsyncUdpReceiver(asyncore.dispatcher):
         self.lpwr = float(msg[1])
         self.rdir = msg[2]
         self.ldir = msg[3]
-        self.drive()
+        if self.rservo and self.lservo:
+            self.drive()
 
     def drive(self):
         self.rservo.set_dir(self.rdir)
         self.lservo.set_dir(self.ldir)
         self.rservo.set_spd(self.rpwr)
         self.lservo.set_spd(self.lpwr)
+        print "driving {0}@{1}  {2}@{3}".format(
+            self.rpwr, self.rdir, self.lpwr, self.ldir)
 
 
 if __name__ == "__main__":
--- motor_driver.py
+++ motor_driver.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import RPi.GPIO as GPIO
+import time
 
 class MotorDriver:
 
@@ -50,5 +51,30 @@ class MotorDriver:
             self.set_fwd()
         else:
             self.set_off()
+
+if __name__ == "__main__":
+
+    PIN_DICT = {'ENA':21, 'ENB':16, 
+                'IN1':13, 'IN2':20, 
+                'IN3':26, 'IN4':19}
+
+    s1_pins = [PIN_DICT[key] for key in ['ENA', 'IN1', 'IN2']]
+    servo1 = MotorDriver(*s1_pins)
+
+    s2_pins = [PIN_DICT[key] for key in ['ENB', 'IN3', 'IN4']]
+    servo2 = MotorDriver(*s2_pins)
+
+    servo1.set_fwd()
+    servo2.set_rev()
+
+    servo1.start(50)
+    servo2.start(50)
+
+    time.sleep(2)
+
+    servo2.set_spd(100)
+    servo1.set_off(100)
+
+    time.sleep(2)
 
 
