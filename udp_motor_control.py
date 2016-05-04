f3ff4c3019e6d7da71f58e9e8d2e3451111db142
Benjamin Pearce <bpearce9@gatech.edu>
2016-05-04 18:51:29 -0400

    moved try block

--- udp_motor_control.py
+++ udp_motor_control.py
@@ -3,21 +3,20 @@ import RPi.GPIO as GPIO
 from motor_driver import MotorDriver
 
 if __name__ == "__main__":
+    PIN_DICT = {'ENA':21, 'ENB':16, 
+                'IN1':13, 'IN2':20, 
+                'IN3':26, 'IN4':19}
+
+    s1_pins = [PIN_DICT[key] for key in ['ENA', 'IN1', 'IN2']]
+    servo1 = MotorDriver(*s1_pins)
+
+    s2_pins = [PIN_DICT[key] for key in ['ENB', 'IN3', 'IN4']]
+    servo2 = MotorDriver(*s2_pins)
+
+    servo1.start(10)
+    servo2.start(10)
 
     try:
-        PIN_DICT = {'ENA':21, 'ENB':16, 
-                    'IN1':13, 'IN2':20, 
-                    'IN3':26, 'IN4':19}
-
-        s1_pins = [PIN_DICT[key] for key in ['ENA', 'IN1', 'IN2']]
-        servo1 = MotorDriver(*s1_pins)
-
-        s2_pins = [PIN_DICT[key] for key in ['ENB', 'IN3', 'IN4']]
-        servo2 = MotorDriver(*s2_pins)
-
-        servo1.start(10)
-        servo2.start(10)
-
         while True:
             pass
 
